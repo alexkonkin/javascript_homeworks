b93026adc04f9734fbaeb756e790ea97701bb9a3
alexkonkin <alexkonkin@gmail.com>
2016-02-15 17:51:50 +0200

    stubs for functions have been added to js file

--- hometask_02/js/hometask_02.js
+++ hometask_02/js/hometask_02.js
@@ -1,4 +1,56 @@
 /**
  * Created by admin on 2/15/2016.
  */
+// Write a function that takes a single argument (a string) and returns the string reversed.
+function reverseString(str) {
+    // FILL THIS IN
+}
 
+// Write a function that takes an array of numbers and returns the minimum value
+function findMinValue(values) {
+    // FILL THIS IN
+}
+
+// Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
+function findDistinctValues(values) {
+    // FILL THIS IN
+}
+
+// Write a function that logs the numbers from 1 to 100 to the console.
+// For multiples of three print "Fizz" instead of the number.
+// For multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+function doFizzBuzz() {
+    // FILL THIS IN
+}
+
+// You have a master array of strings, where each element is a fruit name.
+// You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
+// For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
+// Write the function that will remove the values contained in fruitsToRemove from the array fruits.
+function removeFruits(fruits, fruitsToRemove) {
+    // FILL THIS IN
+}
+
+// Write a function to push either a simple value or an array of values onto a specified array.
+// For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
+// If toPush is a simple value, it should be pushed onto array as an element.
+// If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
+function pushOntoArray(array, toPush) {
+    // FILL THIS IN
+}
+
+// Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
+function splitListStrUsingComma(sourceStr) {
+    // FILL THIS IN
+}
+
+// Write a function that will take any number of arguments and return their sum
+function sum() {
+    // FILL THIS IN
+}
+
+// Write a function that will return true if a specified string consists of only whitespace.
+function isOnlyWhitespace(sourceStr) {
+    // FILL THIS IN
+}
